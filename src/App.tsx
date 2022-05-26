e755b84f3e150bb68d14a592f03410ca53e20d8a
westorres9 <wester.torres9@gmail.com>
2022-05-26 17:31:03 -0300

    projeto inicial criado

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './assets/styles/custom.scss';
+import './App.css';
+import Navbar from 'components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>carrosTop</h1>
+    </>
+  );
+}
+
+export default App;
